d8f485524a20265897173130f962046b40cc8adc
Aleksandr_Titov <aleksandr_titov@epam.com>
2020-02-12 17:58:36 +0400

    add express winston middleware

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -3,6 +3,8 @@ import helmet from 'helmet';
 import cors from 'cors';
 import { Container } from 'inversify';
 import { InversifyExpressServer } from 'inversify-express-utils';
+import winston from 'winston';
+import expressWinston from 'express-winston';
 
 export async function expressLoader(di: Container, callback: () => void) {
   const server = new InversifyExpressServer(di);
@@ -11,6 +13,16 @@ export async function expressLoader(di: Container, callback: () => void) {
     app.use(cors());
     app.use(express.json());
     app.use(helmet());
+    app.use(expressWinston.logger({
+      transports: [
+        new winston.transports.Console(),
+      ],
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.json()
+      ),
+      msg: "HTTP {{req.method}} {{req.url}}",
+    }));
 
     // app.use(errorHandler({ debug: process.env.ENV !== 'prod', log: true }))
   });
